pamaury/rockbox

commit 4be3b442e309c00821be3cb0651e2d2cf44bb6de

Remove the call to settings_save() from settings_load_config() as it seems unneeded any more and causes unecessary writes on every startup.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@28120 a1c6a512-1295-4272-9138-f99709370657

apps/settings.c
@@ -367,7 +367,6 @@ bool settings_load_config(const char* file, bool apply)
     } /* while(...) */
 
     close(fd);
-    settings_save();
     if (apply)
     {
         settings_apply(true);

commit 11b4682e114327478b1e9b6755879fa46f9abc93

Fix FS#11648 - loading a .cfg doesnt automatically save the new config, caused by r28120. This fix will only save the new config loaded from the browsers, not during boot
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@28201 a1c6a512-1295-4272-9138-f99709370657

apps/settings.c
@@ -369,6 +369,7 @@ bool settings_load_config(const char* file, bool apply)
     close(fd);
     if (apply)
     {
+        settings_save();
         settings_apply(true);
         settings_apply_skins();
     }