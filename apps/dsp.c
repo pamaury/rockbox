pamaury/rockbox

commit ba1c3458d1710845663583f83ed88fc3bc0a1752

FS#11964. Rework replaygain handling to save metadata buffer and binsize. Remove string representation of replaygain and use a dedicated ftoa implementation for WPS/screen info.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@29388 a1c6a512-1295-4272-9138-f99709370657

apps/dsp.c
@@ -1475,22 +1475,22 @@ intptr_t dsp_configure(struct dsp_config *dsp, int setting, intptr_t value)
 
     case DSP_SET_TRACK_GAIN:
         if (dsp == &AUDIO_DSP)
-            dsp_set_gain_var(&track_gain, value);
+            dsp_set_gain_var(&track_gain, convert_gain(value));
         break;
 
     case DSP_SET_ALBUM_GAIN:
         if (dsp == &AUDIO_DSP)
-            dsp_set_gain_var(&album_gain, value);
+            dsp_set_gain_var(&album_gain, convert_gain(value));
         break;
 
     case DSP_SET_TRACK_PEAK:
         if (dsp == &AUDIO_DSP)
-            dsp_set_gain_var(&track_peak, value);
+            dsp_set_gain_var(&track_peak, convert_gain(value));
         break;
 
     case DSP_SET_ALBUM_PEAK:
         if (dsp == &AUDIO_DSP)
-            dsp_set_gain_var(&album_peak, value);
+            dsp_set_gain_var(&album_peak, convert_gain(value));
         break;
 
     default:

apps/gui/skin_engine/skin_tokens.c
@@ -35,6 +35,7 @@
 #include "sound.h"
 #include "debug.h"
 #include "cuesheet.h"
+#include "replaygain.h"
 #ifdef HAVE_LCD_CHARCELLS
 #include "hwcompat.h"
 #endif
@@ -1305,8 +1306,8 @@ const char *get_token_value(struct gui_wps *gwps,
             {
                 int type;
                 if (LIKELY(id3))
-                    type = get_replaygain_mode(id3->track_gain_string != NULL,
-                                        id3->album_gain_string != NULL);
+                    type = get_replaygain_mode(id3->track_gain != 0,
+                                               id3->album_gain != 0);
                 else
                     type = -1;
 
@@ -1331,11 +1332,11 @@ const char *get_token_value(struct gui_wps *gwps,
                 /* due to above, coming here with !id3 shouldn't be possible */
                 case 2:
                 case 4:
-                    strlcpy(buf, id3->track_gain_string, buf_size);
+                    replaygain_itoa(buf, buf_size, id3->track_gain);
                     break;
                 case 3:
                 case 5:
-                    strlcpy(buf, id3->album_gain_string, buf_size);
+                    replaygain_itoa(buf, buf_size, id3->album_gain);
                     break;
             }
             return buf;

apps/metadata.c
@@ -411,12 +411,6 @@ void adjust_mp3entry(struct mp3entry *entry, void *dest, const void *orig)
         entry->albumartist += offset;
     if (entry->grouping)
         entry->grouping += offset;
-#if CONFIG_CODEC == SWCODEC
-    if (entry->track_gain_string)
-        entry->track_gain_string += offset;
-    if (entry->album_gain_string)
-        entry->album_gain_string += offset;
-#endif
     if (entry->mb_track_id)
         entry->mb_track_id += offset;
 }

apps/metadata.h
@@ -283,13 +283,10 @@ struct mp3entry {
 #endif
     
     /* replaygain support */
-    
 #if CONFIG_CODEC == SWCODEC
-    char* track_gain_string;
-    char* album_gain_string;
-    long track_gain;    /* 7.24 signed fixed point. 0 for no gain. */
+    long track_gain;    /* s19.12 signed fixed point. 0 for no gain. */
     long album_gain;
-    long track_peak;    /* 7.24 signed fixed point. 0 for no peak. */
+    long track_peak;    /* s19.12 signed fixed point. 0 for no peak. */
     long album_peak;
 #endif
 

apps/metadata/asf.c
@@ -456,18 +456,8 @@ static int asf_parse_header(int fd, struct mp3entry* id3,
                                 lseek(fd, length, SEEK_CUR);
                             }
                         } else if (!strncmp("replaygain_", utf8buf, 11)) {
-                            char* value = id3buf;
-                            int buf_len = id3buf_remaining;
-                            int len;
                             asf_utf16LEdecode(fd, length, &id3buf, &id3buf_remaining);
-                            len = parse_replaygain(utf8buf, value, id3, 
-                                value, buf_len);
-                            
-                            if (len == 0) {
-                                /* Don't need to keep the value */
-                                id3buf = value;
-                                id3buf_remaining = buf_len;
-                            }
+                            parse_replaygain(utf8buf, id3buf, id3);
                         } else if (!strcmp("MusicBrainz/Track Id", utf8buf)) {
                             id3->mb_track_id = id3buf;
                             asf_utf16LEdecode(fd, length, &id3buf, &id3buf_remaining);

apps/metadata/id3tags.c
@@ -370,14 +370,8 @@ static int parseuser( struct mp3entry* entry, char* tag, int bufferpos )
             entry->albumartist = tag;
 #if CONFIG_CODEC == SWCODEC
         } else {
-            /* Calculate residual buffer size in bytes which can be used by 
-             * parse_replaygain() to save the string representation of 
-             * replaygain data.*/
-            length = sizeof(entry->id3v2buf) - (tag - entry->id3v2buf);
-
-            /* Call parse_replaygain(), returns length in bytes used by the
-             * string representation of replaygain data. */
-            length = parse_replaygain(tag, value, entry, tag, length);
+            /* Call parse_replaygain(). */
+            parse_replaygain(tag, value, entry);
 #endif
         }
     }
@@ -387,12 +381,11 @@ static int parseuser( struct mp3entry* entry, char* tag, int bufferpos )
 
 #if CONFIG_CODEC == SWCODEC
 /* parse RVA2 binary data and convert to replaygain information. */
-static int parserva2( struct mp3entry* entry, char* tag, int bufferpos )
+static int parserva2( struct mp3entry* entry, char* tag, int bufferpos)
 {
     int desc_len = strlen(tag);
     int start_pos = tag - entry->id3v2buf;
     int end_pos = start_pos + desc_len + 5;
-    int value_len = 0;
     unsigned char* value = tag + desc_len + 1;
 
     /* Only parse RVA2 replaygain tags if tag version == 2.4 and channel
@@ -447,11 +440,10 @@ static int parserva2( struct mp3entry* entry, char* tag, int bufferpos )
             }
         }
             
-        value_len = parse_replaygain_int(album, gain, peak * 2, entry,
-            tag, sizeof(entry->id3v2buf) - start_pos);
+        parse_replaygain_int(album, gain, peak * 2, entry);
     }
 
-    return start_pos + value_len;
+    return start_pos;
 }
 #endif
 

apps/metadata/metadata_common.c
@@ -333,7 +333,7 @@ long parse_tag(const char* name, char* value, struct mp3entry* id3,
     }
     else
     {
-        len = parse_replaygain(name, value, id3, buf, buf_remaining);
+        parse_replaygain(name, value, id3);
         p = NULL;
     }
     

apps/metadata/mp4.c
@@ -528,13 +528,7 @@ static bool read_mp4_tags(int fd, struct mp3entry* id3,
                         buffer -= length;
                         buffer_left += length;
                         
-                        if (parse_replaygain(tag_name, buffer, id3, 
-                            buffer, buffer_left) > 0)
-                        {
-                            /* Data used, keep it. */
-                            buffer += length;
-                            buffer_left -= length;
-                        }
+                        parse_replaygain(tag_name, buffer, id3);
                     }
                 }
             }

commit 4167d6490acb789cab9c8c7e331e130d1cb5007f

Fix peak handling in replaygain (FS#11981). This bug was introduced with r29388 and is caused by calling convert_gain() for peak values. This results in very low output volume if 'Prevent Clipping' is enabled.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@29509 a1c6a512-1295-4272-9138-f99709370657

apps/dsp.c
@@ -1485,12 +1485,12 @@ intptr_t dsp_configure(struct dsp_config *dsp, int setting, intptr_t value)
 
     case DSP_SET_TRACK_PEAK:
         if (dsp == &AUDIO_DSP)
-            dsp_set_gain_var(&track_peak, convert_gain(value));
+            dsp_set_gain_var(&track_peak, value);
         break;
 
     case DSP_SET_ALBUM_PEAK:
         if (dsp == &AUDIO_DSP)
-            dsp_set_gain_var(&album_peak, convert_gain(value));
+            dsp_set_gain_var(&album_peak, value);
         break;
 
     default: