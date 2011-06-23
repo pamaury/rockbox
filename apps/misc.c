pamaury/rockbox

commit 4dd84283c8222a2db74da137582eb689dd002ba0

Fixed a changelog export/import problem with tagcache where <CR> or <LF> characters in a tag could cause the parser fail to import a track statistics correctly. Now line feeds are escaped properly and carriage returns ignored on import.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30021 a1c6a512-1295-4272-9138-f99709370657

apps/misc.c
@@ -184,15 +184,7 @@ int fast_readline(int fd, char *buf, int buf_size, void *parameters,
         if (rc >= 0)
             buf[pos+rc] = '\0';
 
-        if ( (p = strchr(buf, '\r')) != NULL)
-        {
-            *p = '\0';
-            next = ++p;
-        }
-        else
-            p = buf;
-
-        if ( (p = strchr(p, '\n')) != NULL)
+        if ( (p = strchr(buf, '\n')) != NULL)
         {
             *p = '\0';
             next = ++p;

apps/tagcache.c
@@ -3278,6 +3278,13 @@ static bool write_tag(int fd, const char *tagstr, const char *datastr)
         if (*datastr == '"' || *datastr == '\\')
             buf[i++] = '\\';
         
+        else if (*datastr == '\n')
+        {
+            buf[i++] = '\\';
+            buf[i] = 'n';
+            continue;
+        }
+        
         buf[i] = *(datastr++);
     }
     
@@ -3335,12 +3342,18 @@ static bool read_tag(char *dest, long size,
             
             if (*src == '\\')
             {
-                dest[pos] = *(src+1);
-                src += 2;
+                src++;
+                if (*src == 'n')
+                    dest[pos] = '\n';
+                else
+                    dest[pos] = *src;
+                
+                src++;
                 continue;
             }
             
-            dest[pos] = *src;
+            if (*src == '\0')
+                break;
             
             if (*src == '"')
             {
@@ -3348,10 +3361,7 @@ static bool read_tag(char *dest, long size,
                 break;
             }
             
-            if (*src == '\0')
-                break;
-            
-            src++;
+            dest[pos] = *(src++);
         }
         
         dest[pos] = '\0';
@@ -3377,10 +3387,10 @@ static int parse_changelog_line(int line_n, const char *buf, void *parameters)
     if (*buf == '#')
         return 0;
     
-    logf("%d/%s", line_n, buf);
+    /* logf("%d/%s", line_n, buf); */
     if (!read_tag(tag_data, sizeof tag_data, buf, "filename"))
     {
-        logf("filename missing");
+        logf("%d/filename missing", line_n);
         logf("-> %s", buf);
         return 0;
     }
@@ -3388,13 +3398,13 @@ static int parse_changelog_line(int line_n, const char *buf, void *parameters)
     idx_id = find_index(tag_data);
     if (idx_id < 0)
     {
-        logf("entry not found");
+        logf("%d/entry not found", line_n);
         return 0;
     }
     
     if (!get_index(masterfd, idx_id, &idx, false))
     {
-        logf("failed to retrieve index entry");
+        logf("%d/failed to retrieve index entry", line_n);
         return 0;
     }
     
@@ -3402,7 +3412,7 @@ static int parse_changelog_line(int line_n, const char *buf, void *parameters)
     if (idx.flag & FLAG_DIRTYNUM)
         return 0;
     
-    logf("import: %s", tag_data);
+    logf("%d/import: %s", line_n, tag_data);
     
     idx.flag |= FLAG_DIRTYNUM;
     for (i = 0; i < (long)(sizeof(import_tags)/sizeof(import_tags[0])); i++)

commit 247bd2ef60eaa93484b79e6796c4c828c4e334ea

Fixed a regression caused in r30021: tagnavi_custom.config parsing fails if lines ended with <CR><LF> sequence.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@30058 a1c6a512-1295-4272-9138-f99709370657

apps/misc.c
@@ -170,7 +170,7 @@ bool warn_on_pl_erase(void)
 
 /* Performance optimized version of the read_line() (see below) function. */
 int fast_readline(int fd, char *buf, int buf_size, void *parameters,
-                  int (*callback)(int n, const char *buf, void *parameters))
+                  int (*callback)(int n, char *buf, void *parameters))
 {
     char *p, *next;
     int rc, pos = 0;

apps/misc.h
@@ -57,7 +57,7 @@ bool warn_on_pl_erase(void);
  */
 int read_line(int fd, char* buffer, int buffer_size);
 int fast_readline(int fd, char *buf, int buf_size, void *parameters,
-                  int (*callback)(int n, const char *buf, void *parameters));
+                  int (*callback)(int n, char *buf, void *parameters));
 
 bool settings_parseline(char* line, char** name, char** value);
 long default_event_handler_ex(long event, void (*callback)(void *), void *parameter);

apps/tagcache.c
@@ -3350,7 +3350,7 @@ static bool read_tag(char *dest, long size,
     return false;
 }
 
-static int parse_changelog_line(int line_n, const char *buf, void *parameters)
+static int parse_changelog_line(int line_n, char *buf, void *parameters)
 {
     struct index_entry idx;
     char tag_data[TAG_MAXLEN+32];

apps/tagtree.c
@@ -852,14 +852,20 @@ bool tagtree_import(void)
 
 static bool parse_menu(const char *filename);
 
-static int parse_line(int n, const char *buf, void *parameters)
+static int parse_line(int n, char *buf, void *parameters)
 {
     char data[256];
     int variable;
     static bool read_menu;
     int i;
+    char *p;
     
     (void)parameters;
+    
+    /* Strip possible <CR> at end of line. */
+    p = strchr(buf, '\r');
+    if (p != NULL)
+        *p = '\0';
     
     logf("parse:%d/%s", n, buf);
     